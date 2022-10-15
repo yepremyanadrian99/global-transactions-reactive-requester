09c459ae4486a273c30203f6bb5934d9f43fded3
Yepremyan <adrian.yepremyan@picsart.com>
2022-10-15 22:07:21 +0400

    removed unnecessary annotation.

--- src/main/java/am/adrian/global/transactions/requester/config/MongoDbConfig.java
+++ src/main/java/am/adrian/global/transactions/requester/config/MongoDbConfig.java
@@ -1,10 +1,7 @@
 package am.adrian.global.transactions.requester.config;
 
-import am.adrian.global.transactions.requester.repository.FolderRepository;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @Configuration
-@EnableReactiveMongoRepositories(basePackageClasses = FolderRepository.class)
 public class MongoDbConfig {
 }
