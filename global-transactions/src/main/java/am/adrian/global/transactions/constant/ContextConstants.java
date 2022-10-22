515da830c920799b53bdb55cb9096286f5415e17
Yepremyan <adrian.yepremyan@picsart.com>
2022-10-22 19:26:17 +0400

    updated mapstruct lib version. removed global-transactions library from here and extracted into a separate git repository.

--- global-transactions/src/main/java/am/adrian/global/transactions/constant/ContextConstants.java
+++
@@ -1,9 +0,0 @@
-package am.adrian.global.transactions.constant;
-
-/**
- * Class used to hold all global-transactions-related constants.
- */
-public class ContextConstants {
-
-    public static final String TRANSACTIONAL_CONTEXT = "transaction_context";
-}
